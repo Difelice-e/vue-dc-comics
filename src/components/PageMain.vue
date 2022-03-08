c6c09e68841375d898b0fb1b5dc552a1df8769ff
Difelice-e <difelice.emanuele@gmail.com>
2022-03-08 15:16:29 +0100

    main

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,6 +1,8 @@
 <template>
-    <main>
-        {{ text }}
+    <main class="main">
+        <div class="container">
+            <h2>{{ text }}</h2>
+        </div>
     </main>
 </template>
 
@@ -16,5 +18,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.main {
+    background-color: black;
+    color: white;
+    min-height: 150px;
 
+    h2 {
+        line-height: 120px;
+        padding: 30px 10px;
+    }
+}
 </style>
